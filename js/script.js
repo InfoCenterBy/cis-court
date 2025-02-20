65b13355a45dbe3a5e16cb606cab00e54935e668
dyamorph <dyamorph22@gmail.com>
2025-02-20 12:06:17 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -86,3 +86,19 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const swiperDefault = new Swiper(".swiper.slider-default", {
+    direction: "horizontal",
+    loop: true,
+    allowTouchMove: true,
+    spaceBetween: 1,
+    slidesPerView: 1,
+    centeredSlides: true,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+});
