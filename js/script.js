e686e3c5ddd674251f196f4bf3f815ee951b9b20
dyamorph <dyamorph22@gmail.com>
2025-02-24 16:50:53 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -102,3 +102,21 @@ document.addEventListener("DOMContentLoaded", function () {
     },
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const scrollToTopButton = document.getElementById("scroll-to-top");
+
+  if (scrollToTopButton) {
+    window.onscroll = function () {
+      if (window.scrollY > 300) {
+        scrollToTopButton.style.display = "block";
+      } else {
+        scrollToTopButton.style.display = "none";
+      }
+    };
+
+    scrollToTopButton.addEventListener("click", function () {
+      window.scrollTo(0, 0);
+    });
+  }
+});
